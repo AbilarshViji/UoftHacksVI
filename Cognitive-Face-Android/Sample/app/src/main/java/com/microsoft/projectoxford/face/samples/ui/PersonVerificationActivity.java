286e752472a30e452bb1a435809665418da4af51
AbilarshViji <AbilarshViji@users.noreply.github.com>
2019-01-20 07:59:56 -0500

    karn edits

--- Cognitive-Face-Android/Sample/app/src/main/java/com/microsoft/projectoxford/face/samples/ui/PersonVerificationActivity.java
+++ Cognitive-Face-Android/Sample/app/src/main/java/com/microsoft/projectoxford/face/samples/ui/PersonVerificationActivity.java
@@ -329,8 +329,7 @@ public class PersonVerificationActivity extends AppCompatActivity {
 
         if(resultCode == RESULT_OK) {
             // If image is selected successfully, set the image URI and bitmap.
-            Bitmap bitmap = ImageHelper.loadSizeLimitedBitmapFromUri(
-                    data.getData(), getContentResolver());
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
             if (bitmap != null) {
                 // Image is select but not detected, disable verification button.
                 setVerifyButtonEnabledStatus(false);
